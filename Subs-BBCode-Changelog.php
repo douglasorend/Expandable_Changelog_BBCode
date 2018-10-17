b18ecc2eddd47006f5a69c613454c9fd3686b356
douglasorend <37510276+douglasorend@users.noreply.github.com>
2018-10-17 11:47:28 -0500

    v1.5 - December 30th, 2014
    v1.5 - December 30th, 2014
    o language.xml file renamed to english.xml
    o Added new form of the bbcode: [changelog=Some Text]Content Here[/changelog]
    o Changed bbcode definition code based on bbcodes defined in Sources/Subs.php...

--- Subs-BBCode-Changelog.php
+++ Subs-BBCode-Changelog.php
@@ -10,19 +10,18 @@
 
 function BBCode_Changelog(&$bbc)
 {
+	global $txt;
+
 	$bbc[] = array(
 		'tag' => 'changelog',
-		'type' => 'unparsed_content',
-		'dcontent' => '<div style="padding: 3px; font-size: 1em;"><div style="text-transform: uppercase; border-bottom: 1px solid #5873B0; margin-bottom: 3px; font-size: 0.8em; font-weight: bold; display: block;"><span onClick="if (this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display != \'\') {  this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'\'; this.innerHTML = \'<b>xu7aprafafrA: </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>hide</a>\'; } else { this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'none\'; this.innerHTML = \'<b>xu7aprafafrA: </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>show</a>\'; }" /><b>xu7aprafafrA: </b><a href="#" onClick="return false;">show</a></span></div><div class="quotecontent"><div style="display: none;">$1</div></div></div>',
-		'validate' => isset($disabled['changelog']) ? null : create_function('&$tag, &$data, $disabled', '
-			global $txt;
-			if (!isset($tag["content"]))
-			{
-				$tmp = str_replace("xu7aprafafrA", $txt["changelog"], $tag["dcontent"]);
-				$tmp = str_replace("xu7aprafafrC", $txt["debug_hide"], str_replace("xu7aprafafrB", $txt["debug_show"], $tmp));
-				$tag["content"] = $tmp;
-			}
-		'),
+		'before' => '<div style="padding: 3px; font-size: 1em;"><div style="text-transform: uppercase; border-bottom: 1px solid #5873B0; margin-bottom: 3px; font-size: 0.8em; font-weight: bold; display: block;"><span onClick="if (this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display != \'\') {  this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'\'; this.innerHTML = \'<b>' . $txt['changelog'] . ': </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>' . $txt["debug_hide"] . '</a>\'; } else { this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'none\'; this.innerHTML = \'<b>' . $txt['changelog'] . ': </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>' . $txt["debug_show"] . '</a>\'; }" /><b>' . $txt['changelog'] . ': </b><a href="#" onClick="return false;">' . $txt["debug_show"] . '</a></span></div><div class="quotecontent"><div style="display: none;">',
+		'after' => '</div></div></div>',
+	);
+	$bbc[] = array(
+		'tag' => 'changelog',
+		'type' => 'parsed_equals',
+		'before' => '<div style="padding: 3px; font-size: 1em;"><div style="text-transform: uppercase; border-bottom: 1px solid #5873B0; margin-bottom: 3px; font-size: 0.8em; font-weight: bold; display: block;"><span onClick="if (this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display != \'\') {  this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'\'; this.innerHTML = \'<b>' . $txt['changelog'] . ': $1 &#149; </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>' . $txt["debug_hide"] . '</a>\'; } else { this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'none\'; this.innerHTML = \'<b>' . $txt['changelog'] . ': $1 &#149; </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>' . $txt["debug_show"] . '</a>\'; }" /><b>' . $txt['changelog'] . ': $1 &#149; </b><a href="#" onClick="return false;">' . $txt["debug_show"] . '</a></span></div><div class="quotecontent"><div style="display: none;">',
+		'after' => '</div></div></div>',
 	);
 }
 
